9891ac4dba09292bf0791f4f71528cf1c7c78f88
shirshak001 <23dec022@nith.ac.in.com>
2025-09-16 18:28:49 +0530

    Theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -292,7 +292,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
